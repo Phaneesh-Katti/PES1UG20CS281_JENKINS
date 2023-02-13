62eb080099ea1132f475cb3a59c476b343a40971
Knights_Exalt <pes1ug20cs281@pesu.pes.edu>
2023-02-13 09:55:53 +0530

    first commit PES1UG20CS281 as part of task 2 in A2

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello,PES1UG20CS281_PHANEESH World\n";
+  cout << "Hello,PES1UG20CS281_PHANEESH Jenkins\n";
+  cout << "I, PES1UG20CS281_PHANEESH, have successfully built and run\n";
+  return 0;
+}
