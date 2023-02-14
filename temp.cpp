0768b138213e67673c654d58a98f2b35049176e0
Knights_Exalt <78157559+Phaneesh-Katti@users.noreply.github.com>
2023-02-14 13:00:44 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main(void) {
+int main(void {
   cout << "Hello, World, this is Phaneesh R Katti\n";
   cout << "I am in Task5 my SRN is PES1UG20CS281\n";
   return 0;
